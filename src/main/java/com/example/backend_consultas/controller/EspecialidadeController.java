21ef7a96c5e2c836ba10e5eb99bed7609ae46e04
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-27 09:29:47 -0300

    feat: criação do controller de Especialidade

---
+++ src/main/java/com/example/backend_consultas/controller/EspecialidadeController.java
@@ -0,0 +1,26 @@
+package com.example.backend_consultas.controller;
+import com.example.backend_consultas.model.Especialidade;
+import com.example.backend_consultas.service.EspecialidadeService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/especialidades")
+@CrossOrigin
+
+public class EspecialidadeController {
+    private final EspecialidadeService service;
+    public EspecialidadeController(EspecialidadeService service) {
+        this.service = service;
+    }
+    @PostMapping
+    public Especialidade criar(@RequestBody Especialidade especialidade) {
+        return service.salvar(especialidade);
+    }
+    @GetMapping
+    public List<Especialidade> listar() {
+        return service.listar();
+    }
+}
