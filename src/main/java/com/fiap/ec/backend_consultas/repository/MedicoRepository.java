804229956228fb839dc2333b717686ead6f1007c
iurycar <iurycardoso2001@gmail.com>
2026-09-01 20:00:57 -0300

    feat: verificação de valores iguais no DB

--- src/main/java/com/fiap/ec/backend_consultas/repository/MedicoRepository.java
+++ src/main/java/com/fiap/ec/backend_consultas/repository/MedicoRepository.java
@@ -1,6 +1,12 @@
 package com.fiap.ec.backend_consultas.repository;
 import com.fiap.ec.backend_consultas.model.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
-public interface MedicoRepository extends JpaRepository<Medico, Long>{
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    Optional<Medico> findByCrm(String crm);
+    List<Medico> findByEspecialidadeId(Long especialidadeId);
+    boolean existsByCrm(String crm);
+    boolean existsByCrmAndIdNot(String crm, Long id);
 }
