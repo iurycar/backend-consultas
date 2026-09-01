8cb22ae5bdf4441d106b6ffb70e632b6b94acf2e
iurycar <iurycardoso2001@gmail.com>
2026-09-01 20:08:28 -0300

    feat: adição do 'NOT NULL' para model Medico

--- src/main/java/com/fiap/ec/backend_consultas/model/Medico.java
+++ src/main/java/com/fiap/ec/backend_consultas/model/Medico.java
@@ -1,22 +1,35 @@
 package com.fiap.ec.backend_consultas.model;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name="medicos")
+@Table(name = "medicos")
 public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private String crm;
+
     @ManyToOne
-    @JoinColumn(name="especialidade_id")
+    @JoinColumn(name = "especialidade_id")
     private Especialidade especialidade;
     private Boolean ativo;
-
-    public Medico(){
+    private Double valorConsulta;
+    
+    public Medico() {
     }
-
+    
     public Medico(Long id, String nome, String crm, Especialidade especialidade, Boolean ativo) {
         this.id = id;
         this.nome = nome;
@@ -24,19 +37,19 @@ public class Medico {
         this.especialidade = especialidade;
         this.ativo = ativo;
     }
-
+    
     public Long getId() {
         return id;
     }
-
+    
     public String getNome() {
         return nome;
     }
-
+    
     public String getCrm() {
         return crm;
     }
-
+    
     public Especialidade getEspecialidade() {
         return especialidade;
     }
@@ -63,5 +76,13 @@ public class Medico {
 
     public void setAtivo(Boolean ativo) {
         this.ativo = ativo;
+    }
+
+    public Double getValorConsulta() {
+        return valorConsulta;
+    }
+
+    public void setValorConsulta(Double valorConsulta) {
+        this.valorConsulta = valorConsulta;
     }
 }
