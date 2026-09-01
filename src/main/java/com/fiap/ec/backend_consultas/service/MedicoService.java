40541bbe4e8a548445f1ee6d03941aa51ad27d02
iurycar <iurycardoso2001@gmail.com>
2026-09-01 20:16:20 -0300

    feat: métodos de validação RecursoNaoEncontrado em MedicoService

--- src/main/java/com/fiap/ec/backend_consultas/service/MedicoService.java
+++ src/main/java/com/fiap/ec/backend_consultas/service/MedicoService.java
@@ -22,7 +22,7 @@ public class MedicoService {
 
     public Medico buscarPorId(Long id) {
         return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Médico não encontrado."));
+                .orElseThrow(() -> new RecursoNaoEncontradoException("Médico não encontrado."));
     }
 
     public Medico salvar(Medico medico) {
